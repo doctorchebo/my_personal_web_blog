5722ae1d0143c3f142dd54366b8965a9662fcdd6
doctorchebo <marcelo.munoz.coaquira@gmail.com>
2022-09-02 10:27:29 -0400

    adding static root

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-knudfr*g%&cvyq74ult*qpp@)7e!^01=a)jcq#%2+%v_pg-w)_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chebo-django-backend.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -153,6 +154,6 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS=['http://localhost:3000', 'http://localhost:3001']
+CORS_ALLOWED_ORIGINS=['http://localhost:3000', 'http://localhost:3001', 'https://chebo.herokuapp.com/', 'chebo.herokuapp.com' ]
 
 AUTH_USER_MODEL = 'authentication.MyUser'
