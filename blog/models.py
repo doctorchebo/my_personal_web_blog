d2508523e11f94103bf4ca8c56ce601578fdd7c1
doctorchebo <marcelo.munoz.coaquira@gmail.com>
2022-09-02 15:43:44 -0400

    adding ordering in posts

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,9 @@ class Post(models.Model):
     category = models.ManyToManyField(Category)
     def __str__(self):
         return f'{self.title}'
+
+    class Meta:
+        ordering = ['-created_at']
 class Comment(models.Model):
     author = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
